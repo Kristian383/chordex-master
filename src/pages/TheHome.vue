1f175a840a92cfd90939d61c76bec2233257c963
Kristian <kristian383@gmail.com>
2021-09-06 01:03:06 +0200

    Home

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="home">
-    <div class="sign-form info">
+    <div class="form">
       <!-- <span><h2>Welcome!</h2></span> -->
       <user-auth></user-auth>
     </div>
-    <div class="home-content">
+    <div class="home-content third">
       <div><h2>Keep track of your songs and store them at one place.</h2></div>
-      <img src=".././assets/home.jpg" alt="" />
-    </div>
-    <!--  -->
-
-    <!-- <div class="info"></div> -->
-    <div class="home-content">
-      <!-- <div><h2>Keep track of your songs and store them at one place.</h2></div> -->
-      <div class="home-photo" id="songs">
-        <img src=".././assets/Screenshot_2.png" alt="" />
+      <div class="photo-div">
+        <img src=".././assets/home.jpg" alt="" />
       </div>
     </div>
-    <div class="home-content">
+    <!--  -->
+    <div class="home-content" id="songs">
+      <img src=".././assets/Screenshot_2.png" alt="" />
+    </div>
+    <div class="smaller">
       <h2>Write information about song:</h2>
       <ul>
         <li><h3>Key</h3></li>
@@ -48,14 +45,20 @@ export default {
   margin: 0 auto;
   height: 100%;
 }
-@media (min-width: 700px) {
+@media (min-width: 1000px) {
   .home {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .third {
+    grid-column: 2/4;padding-top: 26px;
+  }
+  #songs {
+    grid-column: 1 / 3;
   }
 }
 
 /* sign form */
-.sign-form {
+.form {
   background-color: #fff;
   position: relative;
   min-height: 500px;
@@ -66,47 +69,27 @@ export default {
 .home-content {
   text-align: center;
 }
-.home-content ul {
+
+.home-content img {
+  /* display: block; */
+  width: 100%;
+  height: 100%;
+
+  max-height: 500px;
+  object-fit: contain;
+}
+
+.smaller ul {
   list-style: none;
 }
-.home-content li {
+.smaller li {
   padding: 16px;
   display: block;
   width: 260px;
   text-align: start;
   margin: 0 auto;
 }
-.home-content h2 {
+.smaller h2 {
   padding: 32px 16px;
 }
-#songs{
-  grid-column: 1 / 3;
-
-}
-.home-content img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  max-height: 500px;
-  object-fit: contain;
-}
-/* .home-photo {
-}
-@media (min-width: 700px) {
-  .home-content {
-  }
-}
-@media (min-width: 800px) {
-  .home-content {
-  }
-}
-
-@media (min-width: 700px) {
-  .sign-form {
-  }
-}
-@media (min-width: 800px) {
-  .sign-form {
-  }
-} */
 </style>
