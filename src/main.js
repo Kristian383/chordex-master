3360b7c7fe2f30070bbc3f63aaeda974954c5360
Kristian <kristian383@gmail.com>
2021-09-09 23:53:04 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import store from "./store/index.js"
 import { faHeart as HeartRegular, faTimesCircle } from "@fortawesome/free-regular-svg-icons"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faPenSquare, faEye, faSearch, faMusic, faSignOutAlt, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faHeart, faVolumeUp, faArrowLeft, faTrashAlt, faMoon, faSun, faMapPin, faCheckSquare, faSave, faLock, faEnvelope,  } from '@fortawesome/free-solid-svg-icons'
 import { faBars, faPenSquare, faEye, faSearch, faMusic, faSignOutAlt, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faHeart, faVolumeUp, faArrowLeft, faTrashAlt, faMoon, faSun, faMapPin, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote,  } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import {  } from '@fortawesome/free-solid-svg-icons'
@@ -17,7 +16,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //library.add(fas)
 
 
-library.add(faBars, faEye, faSearch, faPenSquare, faMusic, faSignOutAlt, faHeart, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faVolumeUp, faArrowLeft, faTrashAlt, HeartRegular,faMoon,faSun,faMapPin,faTimesCircle,faCheckSquare,faSave,faLock,faEnvelope)
 library.add(faBars, faEye, faSearch, faPenSquare, faMusic, faSignOutAlt, faHeart, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faVolumeUp, faArrowLeft, faTrashAlt, HeartRegular,faMoon,faSun,faMapPin,faTimesCircle,faCheckSquare,faSave,faLock,faEnvelope,faAngleRight,faAngleLeft,faStickyNote)
 //zasad nekoristim play, volume up
 const app = createApp(App);
