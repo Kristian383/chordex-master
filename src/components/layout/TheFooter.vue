db0b66ddc3d1bb2508a41f4a67bab77a0719d1ad
Kristian <kristian383@gmail.com>
2021-09-11 17:36:52 +0200

    Update TheFooter.vue

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-      Footer
-  </div>
+  <footer>
+    <div class="container">
+      <div class="logo"><img src="./../../assets/guitar.svg" alt="Logo" /></div>
+      <div>
+        <ul>
+          <li><router-link to="/home">Home</router-link></li>
+          <li><router-link to="/songs">App</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+        </ul>
+      </div>
+      <!--  -->
+      <p>&copy; TT Ind. 2021</p>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+footer {
+  background-color: #f1f1f1;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+  padding: 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
 
+.logo {
+  width: 50px;
+
+  /* margin: 0 auto; */
+}
+.logo img {
+  width: 100%;
+}
+
+.container ul {
+  gap: 16px;
+  list-style: none;
+  display: flex;
+}
+.container ul li a {
+  text-decoration: none;
+  color: RGB(16, 17, 20);
+  font-weight: 500;
+  font-size: 18px;
+}
 </style>
