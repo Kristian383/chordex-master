f910e3c278a08fd3129fd76059659ab3bc25099a
Kristian <kristian383@gmail.com>
2021-09-13 14:24:14 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,17 +3,11 @@ import App from './App.vue'
 import router from "./router.js"
 import store from "./store/index.js"
 
-
 import { faHeart as HeartRegular, faTimesCircle } from "@fortawesome/free-regular-svg-icons"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faPenSquare, faEye, faSearch, faMusic, faSignOutAlt, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faHeart, faVolumeUp, faArrowLeft, faTrashAlt, faMoon, faSun, faMapPin, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote,  } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import {  } from '@fortawesome/free-solid-svg-icons'
-
-
-//import { fas } from '@fortawesome/free-solid-svg-icons'
-//library.add(fas)
 
 
 library.add(faBars, faEye, faSearch, faPenSquare, faMusic, faSignOutAlt, faHeart, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faVolumeUp, faArrowLeft, faTrashAlt, HeartRegular,faMoon,faSun,faMapPin,faTimesCircle,faCheckSquare,faSave,faLock,faEnvelope,faAngleRight,faAngleLeft,faStickyNote)
