e1ce8956b25d72522c8e6e666d697aac299c7381
Kristian <kristian383@gmail.com>
2021-11-29 18:54:27 +0100

    Update NotFound.vue

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  Not found
+  <h2>Not found</h2>
 </template>
 
 <script>
@@ -9,5 +9,9 @@ export default {
 };
 </script>
 
-<style>
+<style
+scoped>
+h2{
+  text-align: center;
+}
 </style>
