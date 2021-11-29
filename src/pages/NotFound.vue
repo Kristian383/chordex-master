3ece44b661bdb203bfdf4db24963ee25fa1f711d
Kristian <kristian383@gmail.com>
2021-11-29 18:07:48 +0100

    Update NotFound.vue

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,14 +1,10 @@
 <template>
-  <base-card>
   Not found
-  </base-card>
 </template>
 
 <script>
-import BaseCard from "./../components/ui/BaseCard.vue"
 export default {
     components: {
-        BaseCard,
     },
 };
 </script>
