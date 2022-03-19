c56ef34749eac06332554092989f7780bfd4c633
Kristian <kristian383@gmail.com>
2022-03-19 12:49:05 +0100

    Songs by key feature

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import BaseCard from "./components/ui/BaseCard.vue"
 import { faHeart as HeartRegular, faTimesCircle } from "@fortawesome/free-regular-svg-icons"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faPenSquare, faEye, faSearch, faMusic, faSignOutAlt, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faHeart, faVolumeUp, faArrowLeft, faTrashAlt, faMoon, faSun, faMapPin, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote, faArrowUp, faEyeSlash, faLockOpen, faPauseCircle, faMinus, faPlus, faDrum } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faPenSquare, faEye, faSearch, faMusic, faSignOutAlt, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faHeart, faVolumeUp, faArrowLeft, faTrashAlt, faMoon, faSun, faMapPin, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote, faArrowUp, faEyeSlash, faLockOpen, faPauseCircle, faMinus, faPlus, faDrum, faListUl } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBars, faEye, faSearch, faPenSquare, faMusic, faSignOutAlt, faHeart, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faVolumeUp, faArrowLeft, faTrashAlt, HeartRegular, faMoon, faSun, faMapPin, faTimesCircle, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote, faArrowUp, faEyeSlash, faAngleLeft, faAngleRight, faLockOpen, faPauseCircle, faMinus, faPlus, faDrum)
+library.add(faBars, faEye, faSearch, faPenSquare, faMusic, faSignOutAlt, faHeart, faUserAlt, faGuitar, faPlusSquare, faEdit, faStar, faHistory, faClipboard, faClipboardList, faQuestionCircle, faThumbtack, faPlayCircle, faHeadphones, faVolumeUp, faArrowLeft, faTrashAlt, HeartRegular, faMoon, faSun, faMapPin, faTimesCircle, faCheckSquare, faSave, faLock, faEnvelope, faAngleRight, faAngleLeft, faStickyNote, faArrowUp, faEyeSlash, faAngleLeft, faAngleRight, faLockOpen, faPauseCircle, faMinus, faPlus, faDrum, faListUl)
 
 const app = createApp(App);
 
--- src/store/songs/getters.js
+++ src/store/songs/getters.js
@@ -125,6 +125,35 @@ export default {
         }
     },
 
+    getSongsByKey(state) {
+        const keys = state.musicKeys;
+        const sortedSongs = {}
+        const songs = state.songs;
+        if (!keys.length) return
+
+        songs.forEach(song => {
+            if (!song.firstKey) return;
+
+            let [key, quality] = song.firstKey.toLowerCase().split(" ");
+            const songData = { songId: song.songId, songName: song.songName, artist: song.artist, songKey: song.firstKey, imgUrl: song.imgUrl };
+
+            if (quality === "MINOR") {
+                let relativeMajor = keys.filter(item => {
+                    return item.relativeMinor === key
+                })[0].key;
+                // console.log(keys, relativeMajor)
+
+                if (sortedSongs[relativeMajor]) sortedSongs[relativeMajor].push(songData)
+                else sortedSongs[relativeMajor] = [songData]
+
+            } else {
+                if (sortedSongs[key]) sortedSongs[key].push(songData)
+                else sortedSongs[key] = [songData]
+            }
+        })
+        return sortedSongs
+    },
+
     getArtists(state) {
         return state.artists;
     },
